92ad653f990bafaab2f92365bcb254f08e82e8fe
Antonio Angotti <Angotti9@gmail.com>
2019-05-10 10:47:17 -0400

    updated main

--- MidTermLibrary/Program.cs
+++ MidTermLibrary/Program.cs
@@ -22,6 +22,9 @@ namespace MidTermLibrary
                 choice = Console.ReadLine().ToLower();
                 switch (choice)
                 {
+
+
+                    // checking out and adding new books 
                     case "author":
                         Console.Write("Please enter in an Author: ");
                         BookMethods.DisplaySpecific(books, "Author", Console.ReadLine());
@@ -34,6 +37,27 @@ namespace MidTermLibrary
                         Console.Write("Please enter in a Genre");
                         BookMethods.DisplaySpecific(books, "Genre", Console.ReadLine());
                         break;
+                    case "checkout":
+                        BookMethods.ListBooks(books);
+                        Console.WriteLine("What book would you like to checkout? ");
+
+                        BookMethods.BookDue(books.ElementAt(int.Parse(Console.ReadLine())+1));
+                        break;
+                    case "addbook":
+                        Console.WriteLine("What book would you like to add to the library?");
+
+                        Console.WriteLine("Enter a title: ");
+                        string title = Console.ReadLine();
+
+                        Console.WriteLine("Enter an author: ");
+                        string author = Console.ReadLine();
+
+                        Console.WriteLine("Enter a genre: ");
+                        string genre = Console.ReadLine();
+
+                        BookMethods.BookAdd(books, title, author, genre);
+                        break;
+
                     default:
                         BookMethods.DisplaySpecific(books, "All", "");
                         break;
